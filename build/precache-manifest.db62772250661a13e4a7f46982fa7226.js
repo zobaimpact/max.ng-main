5b277082ae070214aab8fbdbd248b2899c7f1e48
Chizoba Gerald Okafor <zobaimpact@gmail.com>
2022-07-07 15:12:45 +0100

    first commit

---
+++ build/precache-manifest.db62772250661a13e4a7f46982fa7226.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8fd00806d5fb67c07679fc816685b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9454d22fd31199cdf5",
+    "url": "/static/css/2.965bb1a4.chunk.css"
+  },
+  {
+    "revision": "774b923cf6e267d78227",
+    "url": "/static/css/main.1546319f.chunk.css"
+  },
+  {
+    "revision": "3b9454d22fd31199cdf5",
+    "url": "/static/js/2.3b9454d2.chunk.js"
+  },
+  {
+    "revision": "78d190f0cdc6397a9097b75fa22bd984",
+    "url": "/static/js/2.3b9454d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774b923cf6e267d78227",
+    "url": "/static/js/main.774b923c.chunk.js"
+  },
+  {
+    "revision": "8b0e46ed4bb3a1fb1569",
+    "url": "/static/js/runtime~main.8b0e46ed.js"
+  },
+  {
+    "revision": "f3b273be4a198ec74209466922e5b53f",
+    "url": "/static/media/astro.f3b273be.svg"
+  },
+  {
+    "revision": "0d5782d6335fff67a063bfb2a13735d1",
+    "url": "/static/media/basket.0d5782d6.svg"
+  },
+  {
+    "revision": "80988809f939d317cf6d95a337c7302a",
+    "url": "/static/media/building.80988809.svg"
+  },
+  {
+    "revision": "d614ecfbd4d357370980f5639da52f88",
+    "url": "/static/media/camera.d614ecfb.svg"
+  },
+  {
+    "revision": "7bbcd10c35ff6212e5189c9959b67b20",
+    "url": "/static/media/dashboard.7bbcd10c.svg"
+  },
+  {
+    "revision": "5c11620740583ae357395ccd1a186c98",
+    "url": "/static/media/genetics.5c116207.svg"
+  },
+  {
+    "revision": "55c37442277101a1a6868a5c3e739004",
+    "url": "/static/media/menu.55c37442.svg"
+  },
+  {
+    "revision": "c7c22d4505ef89cfb279f124379c028d",
+    "url": "/static/media/message.c7c22d45.svg"
+  },
+  {
+    "revision": "7c1906d9daa6d2f541860812e31d964b",
+    "url": "/static/media/notification.7c1906d9.svg"
+  },
+  {
+    "revision": "d49ca54ab468e6e35916eefd47cfa332",
+    "url": "/static/media/profile.d49ca54a.svg"
+  },
+  {
+    "revision": "d6337ec7910ff393e190522226624753",
+    "url": "/static/media/star.d6337ec7.svg"
+  },
+  {
+    "revision": "45f40b22514c0982578e422e20966852",
+    "url": "/static/media/starwars.45f40b22.png"
+  },
+  {
+    "revision": "34d0676708f1caa8e5e6edc7e9150f4d",
+    "url": "/static/media/students.34d06767.svg"
+  },
+  {
+    "revision": "b64a53814444a368e9cde6c8f6fa6865",
+    "url": "/static/media/taxi.b64a5381.svg"
+  },
+  {
+    "revision": "b6941fafd9387f3621529697f22b82f9",
+    "url": "/static/media/ufo.b6941faf.svg"
+  }
+]);
